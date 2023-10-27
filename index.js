d9293487decad49a901eb22468f78770bb0f966c
kittituch promlikoon <kittituch.prom@bumail.net>
2023-10-28 00:36:51 +0700

    add comment from B

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+//add feature 1
 function frature1(){
     alert('from feature 1.');
 }
+//add feature 2
 function feature2(){
     alert('from faeture 2.');
 }
 
+//add feature 3
 function feature3(){
     alert('from feature 3.');
 }
